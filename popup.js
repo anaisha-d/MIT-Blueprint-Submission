70d80c7209e0982096f544060efd9073801755a0
Srinivasan Arumugham <srinivasan2003@outlook.com>
2022-02-20 13:36:05 -0800

    fix to quiz

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+    changeColor.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
@@ -16,11 +16,11 @@ chrome.storage.sync.get("color", ({ color }) => {
 
   // The body of this function will be executed as a content script inside the
   // current page
-  function setPageBackgroundColor() {
+function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
+        document.body.style.backgroundColor = color;
     });
-  }
+}
 
 function generateQuestions() {
     // fetch("http://www.michellemobius.org:9005/qa", {method:"POST")
