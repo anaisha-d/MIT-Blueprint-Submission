824dc69e1c527a9d2ffee91b249373733cd21a0f
unknown <anaishadas@gmail.com>
2022-02-20 14:27:08 -0800

    rough edits to frontend

--- popup.js
+++ popup.js
@@ -12,26 +12,73 @@
 
   // The body of this function will be executed as a content script inside the
   // current page
-  const qna = {"answers": [
+  const qna = [{"answers": [
         "Donetsk and Luhansk",
         "Russian military reservists",
         "the Ukrainian army",
         "Women, children and the elderly"
     ],
     "correct": 3,
-    "question": "Who has been ordered to evacuate to Russia?"}
-q = qna.question
-a1 = qna.answers[0]
-a2 = qna.answers[1]
-a3 = qna.answers[2]
-a4 = qna.answers[3]
-rightanswer = qna.correct
+    "question": "Who has been ordered to evacuate to Russia?"}]
 
-document.getElementById("quizButton").addEventListener("click", displayQuestions(qna));
-function displayQuestions(qna) {
-    alert("hi")
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById("quizButton").addEventListener("click", displayQuestions);
+    });
+    qc = document.getElementById("quizContainer")
+function displayQuestions() {
+    let output = []
+    let answers = []
+    s = document.getElementById("start")
+    s.remove()
+    answers.push(`<input type="radio" id="html" name="1" value="Donetsk and Luhansk">
+      <label for="html"> Donetsk and Luhansk</label><br>">`)
+    answers.push(`<input type="radio" id="html" name="2" value="Russian military reservists">
+      <label for="html">Russian military reservists</label><br>">`)
+    answers.push(`<input type="radio" id="html" name="3" value="the Ukrainian army">
+      <label for="html">the Ukrainian army</label><br>">`)
+    answers.push(`<input type="radio" id="html" name="4" value="Women, children and the elderly">
+      <label for="html">Women, children and the elderly</label><br>">`)
+    output.push(
+        `<div class="question"> Who has been ordered to evacuate to Russia? </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+      qc.innerHTML = output.join('');
 }
 
+// function showResults(){
+
+//     // gather answer containers from our quiz
+//     const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+//     // keep track of user's answers
+//     let numCorrect = 0;
+  
+//     // for each question...
+//     qna.forEach( (currentQuestion, questionNumber) => {
+  
+//       // find selected answer
+//       const answerContainer = answerContainers[questionNumber];
+//       const selector = `input[name=question${questionNumber}]:checked`;
+//       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+//       // if answer is correct
+//       if(userAnswer === currentQuestion.correctAnswer){
+//         // add to the number of correct answers
+//         numCorrect++;
+  
+//         // color the answers green
+//         answerContainers[questionNumber].style.color = 'lightgreen';
+//       }
+//       // if answer is wrong or blank
+//       else{
+//         // color the answers red
+//         answerContainers[questionNumber].style.color = 'red';
+//       }
+//     });
+  
+//     // show number of correct answers out of total
+//     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+//   }
 // function generateQuestions() {
 //     alert("hi")
 //     const requestoptions = {
